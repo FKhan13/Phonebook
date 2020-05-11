aef21bab42876c2c36f2ab3901b121b0f4db19b3
Faiyaaz Khan <faiyaaz.khan13@gmail.com>
2020-05-11 17:07:54 +0200

    Initialise Application

---
+++ Phonebook/ClientApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { FooterComponent } from "./footer/footer.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({appId: "ng-cli-universal"}),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent, pathMatch: "full"},
+    ]),
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
